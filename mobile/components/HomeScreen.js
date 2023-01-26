3502d1ae140f754ebc599682c414fcad447b4a4b
Gimillo <jaimegimillo@gmail.com>
2023-01-26 19:29:52 +0100

    Some more done...

--- mobile/components/HomeScreen.js
+++ mobile/components/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text } from 'react-native';
 import { appStyles } from '../styles/AppStyles.js';
 import { homeStyles } from '../styles/HomeStyles.js';
 
