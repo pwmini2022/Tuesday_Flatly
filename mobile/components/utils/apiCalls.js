61ebbba7774c616098728b73e3b78af81ab38055
ghostorbiter <yuldash.yusupov.2001@gmail.com>
2023-01-27 23:10:19 +0100

    Added api call functions in mobile

---
+++ mobile/components/utils/apiCalls.js
@@ -0,0 +1,142 @@
+const BASE_URL = 'http://localhost:8080'
+
+
+// GET methods
+
+export const getOffers = async (ownerId) => {
+    return await fetch(`${BASE_URL}/offers?${ownerId ? `ownerId=${ownerId}` : ""}`)
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                throw response;
+            }
+        })
+        .catch(error => {
+            console.error(JSON.stringify(error));
+        })
+}
+
+export const getBookings = async (ownerId, offerId) => {
+    return await fetch(`${BASE_URL}/bookings?${ownerId ? `ownerId=${ownerId}&` : ""}${offerId ? `offerId=${offerId}` : ""}`)
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                throw response;
+            }
+        })
+        .catch(error => {
+            console.error(JSON.stringify(error));
+        })
+}
+
+// POST methods
+
+export const postOffer = async (ownerId, offers) => {
+    return await fetch(`${BASE_URL}/offers?ownerId=${ownerId}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(offers)
+    })
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {    
+                throw response;
+            }
+        })
+        .catch(error => {
+            console.error(JSON.stringify(error));
+        })
+}
+
+export const postBooking = async (offerId, bookings) => {
+    return await fetch(`${BASE_URL}/bookings?offerId=${offerId}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(bookings)
+    })
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                throw response;
+            }
+        })
+        .catch(error => {
+            console.error(JSON.stringify(error));
+        })
+}
+
+// PUT methods
+
+export const putOffer = async (offerId, offer) => {
+    return await fetch(`${BASE_URL}/offers?offerId=${offerId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(offer)
+    })
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                throw response;
+            }
+        })
+        .catch(error => {
+            console.error(JSON.stringify(error));
+        })
+}
+
+export const putBooking = async (bookingId, booking) => {
+    await fetch(`${BASE_URL}/bookings?bookingUuid=${bookingId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(booking)
+    })
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                throw response;
+            }
+        })
+        .catch(error => {
+            console.error(JSON.stringify(error));
+        })
+}
+
+// DELETE methods
+
+export const deleteOffer = async (offerId) => {
+    await fetch(`${BASE_URL}/offers?offerId=${offerId}`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                throw response;
+            }
+        })
+        .catch(error => {
+            console.error(JSON.stringify(error));
+        })
+}
+
+export const deleteBooking = async (bookingId) => {
+    await fetch(`${BASE_URL}/bookings?bookingUuid=${bookingId}`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                throw response;
+            }
+        })
+        .catch(error => {
+            console.error(JSON.stringify(error));
+        })
+}
