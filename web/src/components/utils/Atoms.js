3a07194345e01d40a3a9816fae04634fb2fb5326
ghostorbiter <yuldash.yusupov.2001@gmail.com>
2023-01-30 15:30:36 +0100

    xdd

--- web/src/components/utils/Atoms.js
+++ web/src/components/utils/Atoms.js
@@ -4,7 +4,7 @@ import { atom, selector } from 'recoil';
 export const user = atom({
     key: 'user',
     default: {
-        id: 2,
+        id: 0,
         username: '',
         email: ''
     },
@@ -12,12 +12,12 @@ export const user = atom({
 
 export const token = atom({
     key: 'token',
-    default: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJnaG9zdG9yYml0ZXIiLCJpcCI6IjE5NC4yOS4xMzcuMjIiLCJleHAiOjE2NzUxMTgyODUsImlhdCI6MTY3NTAzMTg4NSwidXNlci1hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMDkuMC4wLjAgU2FmYXJpLzUzNy4zNiJ9.ajoh_hy9wZ6DXYURRtJOS_hArsztkM84U5xwNoGr5OkLyfi6fzboOYGoQVAI4ipef6S6pUxJXjNVA0ivKt6-jQ'
+    default: ''
 });
 
 export const logged = atom({
     key: 'logged',
-    default: true
+    default: false
 });
 
 export const numOfItems = atom({
